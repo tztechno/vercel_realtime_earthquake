85de186d250d502917d2613035e8794ec7989913
shun ishii <shun_ishii@shunnoMacBook-puro.local>
2024-09-12 14:26:48 +0900

    2024-09-12

---
+++ pages/NavigationPage.jsx
@@ -0,0 +1,13 @@
+// pages/NavigationPage.jsx
+import React from 'react';
+import Navigation from '../components/Navigation'; // Adjust the path if needed
+
+const NavigationPage = () => {
+    return (
+        <div style={{ padding: '20px', border: '1px solid #ccc' }}>
+            <Navigation />
+        </div>
+    );
+};
+
+export default NavigationPage;
