d680a8e4d1c217049973d8e43a577ae927e1f52c
shun ishii <shun_ishii@shunnoMacBook-puro.local>
2024-09-12 13:58:43 +0900

    2024-09-12

--- components/Navigation.js
+++ components/Navigation.js
@@ -9,10 +9,13 @@ const Navigation = () => {
                 <li><Link href="/page1">All Map</Link></li>
                 <li><Link href="/page2">Latest Map</Link></li>
                 <li><Link href="/page7">Huge Map</Link></li>
+
                 <li><Link href="/page3">Table all</Link></li>
                 <li><Link href="/page4">Table small</Link></li>  
+                <li><Link href="/page6">Table huge</Link></li>  
+                   
                 <li><Link href="/page5">Mag Plot</Link></li>      
-                <li><Link href="/page6">Table huge</Link></li>     
+
                 <li><Link href="/time">Time</Link></li>            
             </ul>
         </nav>
